8311ca0c5ae3da858a32b7d5136390cfac065e76
theunk51 <70076004+theunk51@users.noreply.github.com>
2021-03-29 16:59:52 -0500

    Update codenames.py

--- codenames/codenames.py
+++ codenames/codenames.py
@@ -4,20 +4,22 @@ from argparse import ArgumentParser
 
 
 with open('n.json') as n, open('adjective.json') as a:
-    noun = json.load(n)
-    adj = json.load(a)
+  noun = json.load(n)
+  adj = json.load(a)
 
-    n.close()
-    a.close()
+  n.close()
+  a.close()
 
 def generate(number=None, seperator=' ', color=True):
-    """ Generate a codename with an adjective and noun"""
-    
-    if number:
-        for _ in range(number):
-            print(seperator.join([choice(adj), choice(noun)])
-	else:
-	    for _ in range(20):
-            print(seperator.join([choice(adj), choice(noun)])
-    
-                  
+  """ Generate a codename with an adjective and noun"""
+ 
+  if number != None:
+    for _ in range(number):
+      print(seperator.join([choice(adj), choice(noun)]).title())
+  else:
+    for _ in range(20):
+      print(seperator.join([choice(adj), choice(noun)]).title())
+
+
+  if seperator:
+    print(seperator.join([choice(adj), choice(noun)]).title())
